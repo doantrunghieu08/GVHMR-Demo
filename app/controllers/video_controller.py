a8c74666fd551963e3899d49bc882a2c176b0a37
TrungHieu <trunghieudoan088@gmail.com>
2026-09-07 22:15:00 +0700

    Fix SyntaxError in video_controller.py

--- app/controllers/video_controller.py
+++ app/controllers/video_controller.py
@@ -18,7 +18,7 @@ async def upload_video(video: UploadFile = File(...)):
 
     try:
         with open(file_path, "wb") as buffer:
-            while chunk := await video.read(1MB := 1024 * 1024):
+            while chunk := await video.read(1024 * 1024):
                 buffer.write(chunk)
             
         return {
